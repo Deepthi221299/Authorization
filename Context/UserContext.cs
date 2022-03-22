fe12e6f4e56c3071e350f10863ae092b7df13fcc
Deepthi <2063635@cognizant.com>
2022-03-23 03:02:06 +0530

    first commit

---
+++ Context/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using AuthorizationAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationAPI.Context
+{
+    public class UserContext: DbContext
+    {
+        public UserContext() { }
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        { }
+
+        public virtual DbSet<UserCredentials> Users { get; set; }
+
+        
+    }
+}
